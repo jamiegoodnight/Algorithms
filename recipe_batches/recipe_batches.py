02978ba454f420d8b40fb8537e86c9aa4e9e6162
Jamie Goodnight <goodnightlambda@gmail.com>
2019-11-07 01:06:13 -0500

    :construction: Batches complete.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,30 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches = 0
+    if len(recipe) != len(ingredients):
+        return batches
+
+    for key in recipe:
+        if batches > 0 and ingredients[key]/recipe[key] > batches:
+            break
+
+        if recipe[key] <= ingredients[key]:
+            batches = ingredients[key]/recipe[key]
+
+        if recipe[key] > ingredients[key]:
+            print("We're in 2", key)
+            batches = 0
+
+    return round(batches)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+        # Change the entries of these dictionaries to test
+        # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
